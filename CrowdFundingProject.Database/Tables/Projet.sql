46ea68cbf109da5665445e76e7c49e8ac8b257c1
Morre Thierry <thierry.morre@cognitic.be>
2022-09-08 09:24:04 +0200

    Ajoutez des fichiers projet.

---
+++ CrowdFundingProject.Database/Tables/Projet.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Projet]
+(
+	[Id] INT NOT NULL IDENTITY,
+	[Nom] NVARCHAR(256) NOT NULL,
+	[Objectif] INT NOT NULL,
+	[UtilisateurId] INT NOT NULL,
+	CONSTRAINT PK_Projet PRIMARY KEY (Id),
+	CONSTRAINT CK_Projet_Objectif CHECK (Objectif > 0),
+	CONSTRAINT FK_Projet_Utilisateur FOREIGN KEY ([UtilisateurId]) REFERENCES [Utilisateur]([Id])
+);
---
+++ CrowdFundingProject.Database/Tables/Utilisateur.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Utilisateur]
+(
+	[Id] INT NOT NULL, 
+	[Nom] NVARCHAR(75) NOT NULL,
+	[Prenom] NVARCHAR(75) NOT NULL,
+	[Email] NVARCHAR(384) NOT NULL,
+	[Passwd] BINARY(64) NOT NULL,
+    CONSTRAINT [PK_Utilisateur] PRIMARY KEY ([Id])
+);
