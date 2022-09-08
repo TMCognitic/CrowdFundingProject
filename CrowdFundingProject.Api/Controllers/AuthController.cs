a54e5585ba2de597c43de7853fb7edf608099e03
Morre Thierry <thierry.morre@cognitic.be>
2022-09-08 10:10:58 +0200

    Ajout De l'enregestrement dand l'api Ajout de le l'outil de connexion

---
+++ CrowdFundingProject.Api/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using CrowdFunding.Tools.Database;
+using CrowdFundingProject.Api.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+
+namespace CrowdFundingProject.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly DbConnection _connection;
+
+        public AuthController(ILogger<AuthController> logger, DbConnection connection)
+        {
+            _logger = logger;
+            _connection = connection;
+        }
+
+        [HttpPost]
+        public IActionResult AjoutUtilisateur([FromBody] AjoutUtilisateurForm form)
+        {
+            _logger.LogInformation("Nouvel Enregistrement demandé");
+
+            try
+            {
+                _connection.ExecuteNonQuery("CSP_AjoutUtilisateur", true, new { Nom = form.Nom, Prenom = form.Prenom, Email = form.Email, Passwd = form.Passwd });
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                return BadRequest(new { Message = ex.Message });
+#else
+                return BadRequest(new { Message = "Quelque chose n'a pas fonctionné lors de l'enregistrement... Contactez l'administrateur du site" });
+#endif
+
+            }
+        }
+
+    }
+}
---
+++ CrowdFundingProject.Api/Models/Forms/AjoutUtilisateurForm.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrowdFundingProject.Api.Models.Forms
+{
+#nullable disable
+    public class AjoutUtilisateurForm
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(75)]
+        public string Nom { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(75)]
+        public string Prenom { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(384)]
+        public string Email { get; set; }
+        [Required]
+        //[RegularExpression("Expression pour le mot de passe")]
+        [MaxLength(20)]
+        public string Passwd { get; set; }
+    }
+}
