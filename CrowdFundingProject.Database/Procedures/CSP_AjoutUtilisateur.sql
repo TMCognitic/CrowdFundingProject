febf147fd3719f0c2afd465f8791bdbdda2b2068
Morre Thierry <thierry.morre@cognitic.be>
2022-09-08 09:33:41 +0200

    Ajout Enregistrement Utilisateur en procédure stockées

---
+++ CrowdFundingProject.Database/Procedures/CSP_AjoutUtilisateur.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[CSP_AjoutUtilisateur]
+	@Nom NVARCHAR(75),
+	@Prenom NVARCHAR(75),
+	@Email NVARCHAR(384),
+	@Passwd NVARCHAR(20)
+AS
+BEGIN
+	INSERT INTO [Utilisateur] (Nom, Prenom, Email, Passwd) VALUES (@Nom, @Prenom, @Email, HASHBYTES('SHA2_512', dbo.CSF_GetSalt() + @Passwd));
+END
